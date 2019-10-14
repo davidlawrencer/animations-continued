83847308933cb189d05fb4cfad539da66bdec795
davidlawrencer <davidlrifkin+github@gmail.com>
2019-10-14 12:23:05 -0400

    Adds key frame button and beginning of animation. TODO: Make leaf-like motion

--- animations-continued/FirstViewController.swift
+++ animations-continued/FirstViewController.swift
@@ -24,24 +24,76 @@ class FirstViewController: UIViewController {
         return button
     }()
     
+    var keyButton:UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .black
+        button.setTitle("Key", for: .normal)
+        button.addTarget(self, action: #selector(keyButtonPressed), for: .touchUpInside)
+        return button
+    }()
+    
+    lazy var myViewCenterYConstraint: NSLayoutConstraint = {
+        myView.centerYAnchor.constraint(equalTo: self.view.centerYAnchor)
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .white
-        self.view.addSubview(myView)
-        self.view.addSubview(flipButton)
-        // Do any additional setup after loading the view.
-        setUpMyViewConstraints()
-        setUpFlipButtonConstraints()
+        setUpSubviews()
+        setUpConstraints()
+    }
+    
+    @objc private func keyButtonPressed() {
+        UIView.animateKeyframes(withDuration: 1.2, delay: 0, options: [.calculationModePaced], animations: {
+        //TODO: - keyframe animation that has the view "fall down" but sway side to side as it does that (like a leaf)
+
+            //old code (grow, twist, make into rectangle
+//            UIView.addKeyframe(withRelativeStartTime: 0.0, relativeDuration: 0.4) {
+//                self.myView.transform = CGAffineTransform(scaleX: 2, y: 2)
+//            }
+//            UIView.addKeyframe(withRelativeStartTime: 0.4, relativeDuration: 0.4) {
+//                self.myView.transform = CGAffineTransform(rotationAngle: 45)
+//            }
+//            UIView.addKeyframe(withRelativeStartTime: 0.8, relativeDuration: 0.4) {                self.myView.transform = CGAffineTransform(scaleX: 2, y: 1)
+//            }
+//        }, completion: { _ in
+//            UIView.animate(withDuration: 0.4, animations: {
+//                self.myView.alpha = 0.0
+//            })
+        })
     }
     
     @objc private func flipButtonPressed() {
-        UIView.transition(with: myView, duration: 1.2, options: [.autoreverse, .transitionCurlUp,.curveEaseIn], animations: {
+        UIView.transition(with: myView, duration: 1.2, options: [ .transitionCurlUp,.curveEaseIn], animations: {
             if self.myView.backgroundColor == .blue {
                 self.myView.backgroundColor = .red
             } else {
                 self.myView.backgroundColor = .blue
             }
         }, completion: nil)
+    }
+    
+    private func setUpSubviews() {
+        self.view.addSubview(myView)
+        self.view.addSubview(flipButton)
+        self.view.addSubview(keyButton)
+
+    }
+    
+    private func setUpConstraints(){
+        setUpMyViewConstraints()
+        setUpFlipButtonConstraints()
+        setUpKeyButtonConstraints()
+    }
+    
+    private func setUpKeyButtonConstraints() {
+        keyButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            keyButton.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: -100),
+            keyButton.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 50),
+            keyButton.widthAnchor.constraint(equalToConstant: 75),
+            keyButton.heightAnchor.constraint(equalToConstant: 75)
+        ])
     }
     
     private func setUpFlipButtonConstraints(){
@@ -57,7 +109,7 @@ class FirstViewController: UIViewController {
     private func setUpMyViewConstraints(){
         myView.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            myView.centerYAnchor.constraint(equalTo: self.view.centerYAnchor),
+            myViewCenterYConstraint,
             myView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
             myView.widthAnchor.constraint(equalToConstant: 150),
             myView.heightAnchor.constraint(equalToConstant: 150)
